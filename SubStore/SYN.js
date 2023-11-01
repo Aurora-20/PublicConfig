2ffc71a48e441ce3be0cd1bdaa572d696499bc84
Aurora <sllxy20@hotmail.com>
2023-11-01 15:10:29 +0800

    test

--- SubStore/SYN.js
+++ SubStore/SYN.js
@@ -8,174 +8,18 @@
  *
  */
 
-(function (_0x2f529a, _0x413c88) {
-  var _0x342df7 = _0x366e,
-    _0x598adf = _0x2f529a();
-  while (!![]) {
-    try {
-      var _0x288385 =
-        (parseInt(_0x342df7(0xe9)) / 0x1) * (-parseInt(_0x342df7(0xef)) / 0x2) +
-        (parseInt(_0x342df7(0xeb)) / 0x3) * (parseInt(_0x342df7(0xec)) / 0x4) +
-        parseInt(_0x342df7(0xd8)) / 0x5 +
-        (-parseInt(_0x342df7(0xe7)) / 0x6) *
-          (-parseInt(_0x342df7(0xdb)) / 0x7) +
-        (parseInt(_0x342df7(0xe3)) / 0x8) * (-parseInt(_0x342df7(0xe8)) / 0x9) +
-        -parseInt(_0x342df7(0xde)) / 0xa +
-        (-parseInt(_0x342df7(0xea)) / 0xb) * (-parseInt(_0x342df7(0xdd)) / 0xc);
-      if (_0x288385 === _0x413c88) break;
-      else _0x598adf["push"](_0x598adf["shift"]());
-    } catch (_0x290007) {
-      _0x598adf["push"](_0x598adf["shift"]());
-    }
-  }
-})(_0x2fcc, 0xb36b5);
-var _0x51a417 = (function () {
-    var _0x1fcd6f = !![];
-    return function (_0x2ffc92, _0x434854) {
-      var _0x2516f0 = _0x1fcd6f
-        ? function () {
-            if (_0x434854) {
-              var _0x3467d0 = _0x434854["apply"](_0x2ffc92, arguments);
-              return (_0x434854 = null), _0x3467d0;
-            }
-          }
-        : function () {};
-      return (_0x1fcd6f = ![]), _0x2516f0;
-    };
-  })(),
-  _0x4c0791 = _0x51a417(this, function () {
-    var _0x5930aa = _0x366e;
-    return _0x4c0791[_0x5930aa(0xd6)]()
-      [_0x5930aa(0xda)]("(((.+)+)+)+$")
-      [_0x5930aa(0xd6)]()
-      ["constructor"](_0x4c0791)
-      ["search"](_0x5930aa(0xdc));
-  });
-function _0x366e(_0x579daa, _0x4c2dc9) {
-  var _0x51cc01 = _0x2fcc();
-  return (
-    (_0x366e = function (_0x4da229, _0x1898d9) {
-      _0x4da229 = _0x4da229 - 0xd5;
-      var _0x4f693f = _0x51cc01[_0x4da229];
-      return _0x4f693f;
-    }),
-    _0x366e(_0x579daa, _0x4c2dc9)
+function operator(proxies) {
+  proxies.push(
+    Object.assign({}, proxies[0], {
+      port: 50009,
+      name: "🇱🇺 LU 硝酸二甲酯 0.01x",
+    })
   );
-}
-_0x4c0791();
-var _0x1898d9 = (function () {
-    var _0x5bb287 = !![];
-    return function (_0x1f79f5, _0x523231) {
-      var _0x499dcf = _0x5bb287
-        ? function () {
-            var _0x298dfd = _0x366e;
-            if (_0x523231) {
-              var _0x5e4306 = _0x523231[_0x298dfd(0xd7)](_0x1f79f5, arguments);
-              return (_0x523231 = null), _0x5e4306;
-            }
-          }
-        : function () {};
-      return (_0x5bb287 = ![]), _0x499dcf;
-    };
-  })(),
-  _0x4da229 = _0x1898d9(this, function () {
-    var _0x4cf3dc = _0x366e,
-      _0x1b33dd = function () {
-        var _0x4ce1df;
-        try {
-          _0x4ce1df = Function(
-            "return\x20(function()\x20" +
-              "{}.constructor(\x22return\x20this\x22)(\x20)" +
-              ");"
-          )();
-        } catch (_0xc1c892) {
-          _0x4ce1df = window;
-        }
-        return _0x4ce1df;
-      },
-      _0x8f900f = _0x1b33dd(),
-      _0x19fe2d = (_0x8f900f[_0x4cf3dc(0xe0)] =
-        _0x8f900f[_0x4cf3dc(0xe0)] || {}),
-      _0x15b823 = [
-        "log",
-        _0x4cf3dc(0xf1),
-        _0x4cf3dc(0xed),
-        _0x4cf3dc(0xdf),
-        _0x4cf3dc(0xe6),
-        "table",
-        _0x4cf3dc(0xe5),
-      ];
-    for (
-      var _0x3f73f7 = 0x0;
-      _0x3f73f7 < _0x15b823[_0x4cf3dc(0xd9)];
-      _0x3f73f7++
-    ) {
-      var _0x1cac7a =
-          _0x1898d9[_0x4cf3dc(0xf2)][_0x4cf3dc(0xd5)][_0x4cf3dc(0xee)](
-            _0x1898d9
-          ),
-        _0x25ed13 = _0x15b823[_0x3f73f7],
-        _0x1734f3 = _0x19fe2d[_0x25ed13] || _0x1cac7a;
-      (_0x1cac7a[_0x4cf3dc(0xe1)] = _0x1898d9[_0x4cf3dc(0xee)](_0x1898d9)),
-        (_0x1cac7a[_0x4cf3dc(0xd6)] =
-          _0x1734f3[_0x4cf3dc(0xd6)][_0x4cf3dc(0xee)](_0x1734f3)),
-        (_0x19fe2d[_0x25ed13] = _0x1cac7a);
-    }
-  });
-_0x4da229();
-function _0x2fcc() {
-  var _0x4b7226 = [
-    "246zuPjHQ",
-    "22edqdTB",
-    "3AytqDK",
-    "2093260GtuDdP",
-    "info",
-    "bind",
-    "358LzMTkZ",
-    "🇱🇺\x20LU\x20硝酸二甲酯\x200.01x",
-    "warn",
-    "constructor",
-    "prototype",
-    "toString",
-    "apply",
-    "2949765kGhMts",
-    "length",
-    "search",
-    "198877FdcEty",
-    "(((.+)+)+)+$",
-    "5784888BocYoK",
-    "7379070gMaech",
-    "error",
-    "console",
-    "__proto__",
-    "assign",
-    "942376tvBAvK",
-    "push",
-    "trace",
-    "exception",
-    "6bpSLYN",
-    "45bGFVKf",
-  ];
-  _0x2fcc = function () {
-    return _0x4b7226;
-  };
-  return _0x2fcc();
-}
-function operator(_0x1a4a80) {
-  var _0x4a3cad = _0x366e;
-  return (
-    _0x1a4a80["push"](
-      Object[_0x4a3cad(0xe2)]({}, _0x1a4a80[0x0], {
-        port: 0xc359,
-        name: _0x4a3cad(0xf0),
-      })
-    ),
-    _0x1a4a80[_0x4a3cad(0xe4)](
-      Object[_0x4a3cad(0xe2)]({}, _0x1a4a80[0x0], {
-        port: 0xc35c,
-        name: "🇸🇬\x20SG\x20苯巴比妥钠\x200.01x",
-      })
-    ),
-    _0x1a4a80
+  proxies.push(
+    Object.assign({}, proxies[0], {
+      port: 50012,
+      name: "🇸🇬 SG 苯巴比妥钠 0.01x",
+    })
   );
+  return proxies;
 }
