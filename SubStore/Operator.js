e2b73c89790e74abaaa829f9d8c275b4514bf288
Aurora <sllxy20@hotmail.com>
2023-11-02 01:34:39 +0800

    注释更新

--- SubStore/Operator.js
+++ SubStore/Operator.js
@@ -1,9 +1,9 @@
 /**
  * Update : 2023/11/01 14:55:44
- * 1. 本脚本已加密；
- * 2. 功能：为 SYN 已订阅用户快速在 SubStore 中添加低倍率节点；
- * 3. 感谢 @baixiaofei233 提供的思路；
- * 4. 请在 SubStore 中使用，具体使用：编辑->脚本操作->类型（链接）->
+ *
+ * 1. 功能：为 SYN 已订阅用户快速在 SubStore 中添加低倍率节点；
+ * 2. 感谢 @baixiaofei233 提供的思路；
+ * 3. 请在 SubStore 中使用，具体使用：编辑->脚本操作->类型（链接）->
  * 填入本脚本链接（https://github.com/Aurora-20/PublicConfig/raw/main/SubStore/Operator.js）；
  *
  */
