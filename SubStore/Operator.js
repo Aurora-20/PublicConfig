2e6285d587f2fdcc64021487a52f9b761cfdebda
Aurora <sllxy20@hotmail.com>
2023-11-02 21:57:16 +0800

    更换节点信息

--- SubStore/Operator.js
+++ SubStore/Operator.js
@@ -10,13 +10,13 @@
 
 function operator(proxies) {
   proxies.push(
-    Object.assign({}, proxies[0], {
+    Object.assign({}, proxies[proxies.length - 1], {
       port: 50009,
       name: "🇱🇺 LU 硝酸二甲酯 0.01x",
     })
   );
   proxies.push(
-    Object.assign({}, proxies[0], {
+    Object.assign({}, proxies[proxies.length - 1], {
       port: 50012,
       name: "🇸🇬 SG 苯巴比妥钠 0.01x",
     })
