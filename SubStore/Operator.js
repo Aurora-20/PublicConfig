0384b3b45c9473b2dd24a1e34780ddbdacce4007
Aurora <sllxy20@hotmail.com>
2023-11-05 14:50:55 +0800

    更新节点设置

--- SubStore/Operator.js
+++ SubStore/Operator.js
@@ -9,23 +9,23 @@
  */
 
 function operator(proxies) {
-  const proxy = Object.assign({}, proxies[0], {
-    server: "traffic-in-02.811920.xyz",
-  });
   proxies.push(
-    Object.assign({}, proxy, {
+    Object.assign({}, proxies[0], {
+      server: "traffic-in-01.811920.xyz",
       port: 50009,
       name: "🇱🇺 LU 硝酸二甲酯 0.01x",
     })
   );
   proxies.push(
-    Object.assign({}, proxy, {
+    Object.assign({}, proxies[0], {
+      server: "traffic-in-01.811920.xyz",
       port: 50012,
       name: "🇸🇬 SG 苯巴比妥钠 0.01x",
     })
   );
   proxies.push(
-    Object.assign({}, proxy, {
+    Object.assign({}, proxies[0], {
+      server: "traffic-in-02.811920.xyz",
       port: 50013,
       name: "🇭🇰 HK 丁香酰氧胺 x 0.01",
     })
